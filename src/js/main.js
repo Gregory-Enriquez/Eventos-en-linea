acdc2625229db10207cfe7cdd59fba204c37243d
Gregory <est.ggomeze0201@uml.edu.ni>
2023-04-23 17:41:30 -0600

    Refactoring

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+// Librerias
+import 'flowbite'
+
+// Estilos de la pagina
+import '../css/style.css'
+
+
+//Menu de navegacion
+import './menu'
+
+// Importar el header
+import './header'
+
+// carousel
+import './carousel'
